d0187c6468e2d12bb9aacce758d6cf3c8621078d
CaseySMiller <CaseySMiller@yahoo.com>
2023-03-23 22:04:33 -0600

    split reducers and install redux

--- src/recucer.js
+++ src/recucer.js
@@ -1,64 +1,10 @@
-const intialState = {
-    todos: [
-        { id: 0, text: 'Learn React', completed: true },
-        { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-        { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-    ],
-    filters: {
-        status: "All",
-        colors: [],
-    }
-};
+import todosReducer from "./features/todos/todosSlice";
+import filtersReducer from "./features/filters/filtersSlice";
 
-function nextTodoId(todos) {
-    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
-    return maxId + 1;
+export default function rootReducer(state = {}, action) {
+    return {
+        todos: todosReducer(state.todos, action),
+        filters: filtersReducer(state.filters, action),
+    }
 }
-
-export default function appReducer(state = intialState, action) {
-
-    switch (action.type) {
-        //actions here
-        case 'todos/todoAdded': {
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: nextTodoId(state.todos),
-                        text: action.payload,
-                        completed: false
-                    }
-                ]
-            }
-        }
-        case 'todos/todoToggled': {
-            return {
-                ...state,
-                todos: state.todos.map(todo => {
-                    if (todo.id !== action.payload) {
-                        return todo;
-                    }
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    }
-                })
-            }
-        }
-        case 'filters/statusFilterChanged': {
-            return {
-                ...state,
-                filters: {
-                    ...state.filters,
-                    status: action.payload
-                }
-            }
-        }
-        //---------------stopped at "State, Actions, Reducers"/"Splitting Reducers"----------------//
-
-        default:
-            return state
-    }
-};
 
