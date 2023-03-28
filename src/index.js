1bdf881b2cae751859e5fc0477855ac3bc1f6a60
CaseySMiller <CaseySMiller@yahoo.com>
2023-03-27 21:43:20 -0600

    setup files for 3rd part of tutorial

--- src/index.js
+++ src/index.js
@@ -2,41 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import store from './store'
 
 import './api/server'
 
-console.log('initial state: ', store.getState())
-// {todos: [....],  filters: {status, colors}}
+import store from './store'
 
-const unsubscribe = store.subscribe(() => 
-  console.log('State after dispatch: ', store.getState())
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
-
-//dipatch some stuff
-store.dispatch({ type: 'todos/todoAdded', payload: 'arrrr this be a todo' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'arrrr this be another todo' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'arrrr this be a third todo' })
-
-// store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-// store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-// store.dispatch({ type: 'filters/statusFilterChanged', payload: "Active" })
-
-// store.dispatch({ 
-//   type: 'filters/colorFilterChanged', 
-//   payload: { color: 'red', changeType: 'added' }
-// })
-
-//stop listening
-unsubscribe();
-
-// store.dispatch({ type: 'todos/todoAdded', payload: 'bet this wont work' })
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
